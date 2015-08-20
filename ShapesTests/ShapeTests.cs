ac9e09cf852da4bd9de5368d8ae96bcb9c390deb
Nicole <Nicole.Ahima@gmail.com>
2015-08-19 19:47:57 -0500

    Initial Commit

---
+++ ShapesTests/ShapeTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shapes;
+using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestShapes
+{
+    [TestClass]
+    public class AbstractShapeTests
+    {
+        private class MyMockShape : Shape
+        {
+            public override float Area()
+            {
+                throw new NotImplementedException();
+            }
+            public override float Perimeter()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        MyMockShape omgMyShape = new MyMockShape();
+
+        [TestMethod()]
+        public void TestThatAbstractShapeClassHasFillColor()
+        {
+            omgMyShape.FillColor = Color.PapayaWhip;
+            Assert.Fail();
+        }
+    }
+}
